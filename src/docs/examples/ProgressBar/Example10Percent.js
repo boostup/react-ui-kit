fec78e0e8a3deadd58f46f4427267e75dc83e4ab
Fred B <boostup@gmail.com>
2017-12-02 17:02:33 +0100

    some demo components

---
+++ src/docs/examples/ProgressBar/Example10Percent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ProgressBar from 'react-ui-kit/ProgressBar';
+
+/** 10% progress */
+export default function Example10Percent() {
+  return <ProgressBar percent={10} width={150} />
+}

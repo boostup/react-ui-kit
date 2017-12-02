e6dc091803507854acb300a73b32ef83b62a902f
Fred B <boostup@gmail.com>
2017-12-02 17:07:49 +0100

    some purposeful test error to see if travis cancels the merging of the pull request

--- src/components/ProgressBar/ProgressBar.spec.js
+++ src/components/ProgressBar/ProgressBar.spec.js
@@ -7,6 +7,6 @@ describe('ProgressBar', () => {
   test('getWidthAsPercentOfTotalWidth should return 250 with total of 500 and percent of 50', () => {
     const wrapper = shallow(<ProgressBar percent={50} width={500} />)
     const width = wrapper.instance().getWidthAsPercentOfTotalWidth()
-    expect(width).toEqual(250)
+    expect(width).toEqual(2250)
   })
 })
